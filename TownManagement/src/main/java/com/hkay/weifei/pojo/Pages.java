eab0e781079eb4d5325c1bcee6687ffccde86f10
15757821781 <594672337@qq.com>
2017-01-19 15:37:45 +0800

    add pages.java

---
+++ TownManagement/src/main/java/com/hkay/weifei/pojo/Pages.java
@@ -0,0 +1,51 @@
+package com.hkay.weifei.pojo;
+
+public class Pages {
+	private String pagename;
+	private int pagelevel;
+	private int pagegroup;
+	private int pageid;
+	private String url;
+	
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public int getPagelevel() {
+		return pagelevel;
+	}
+
+	public void setPagelevel(int pagelevel) {
+		this.pagelevel = pagelevel;
+	}
+
+	public int getPageid() {
+		return pageid;
+	}
+
+	public void setPageid(int pageid) {
+		this.pageid = pageid;
+	}
+
+	public String getPagename() {
+		return pagename;
+	}
+
+	public void setPagename(String pagename) {
+		this.pagename = pagename;
+	}
+
+	public int getPagegroup() {
+		return pagegroup;
+	}
+
+	public void setPagegroup(int pagegroup) {
+		this.pagegroup = pagegroup;
+	}
+
+
+}
