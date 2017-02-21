92ee0a8b75df5e71738df342d3725a27f5103f50
15757821781 <594672337@qq.com>
2017-02-21 16:00:57 +0800

    新增项目验证

---
+++ TownManagement/src/main/java/com/hkay/weifei/pojo/Tb_chubeixiangmu.java
@@ -0,0 +1,158 @@
+package com.hkay.weifei.pojo;
+
+public class Tb_chubeixiangmu {
+	private int resid;
+	private String resitemname;//政府储备项目名称
+	private int resnumber;//项目编号
+	private String resarea;//所属地区		
+	private String reslocal;//所在镇/街道/开发区
+	private String resjoinway;//合作方式
+	private String rescompetentunit;//主管单位
+	private String restrade;//所属行业
+	private double reslandscale;//用地规模
+	private double resvolumerate;//容积率
+	private String resplanuses;//规划用途
+	private double resinvest;//投资强度
+	private double resgrossassets;//投资总额
+	private double resplantime;//预计建设期
+	private String resplanaddress;//规划地址
+	private String resplanideas;//规划设想
+	private String resbuildterm;//建设条件
+	private String respolicy;//优惠政策
+	private String reselement;//要素分析
+	private String rescontactunit;//联系单位
+	private String rescontacts;//联系人
+	private String rescontactway;//联系方式
+	public int getResid() {
+		return resid;
+	}
+	public void setResid(int resid) {
+		this.resid = resid;
+	}
+	public String getResitemname() {
+		return resitemname;
+	}
+	public void setResitemname(String resitemname) {
+		this.resitemname = resitemname;
+	}
+	public int getResnumber() {
+		return resnumber;
+	}
+	public void setResnumber(int resnumber) {
+		this.resnumber = resnumber;
+	}
+	public String getResarea() {
+		return resarea;
+	}
+	public void setResarea(String resarea) {
+		this.resarea = resarea;
+	}
+	public String getReslocal() {
+		return reslocal;
+	}
+	public void setReslocal(String reslocal) {
+		this.reslocal = reslocal;
+	}
+	public String getResjoinway() {
+		return resjoinway;
+	}
+	public void setResjoinway(String resjoinway) {
+		this.resjoinway = resjoinway;
+	}
+	public String getRescompetentunit() {
+		return rescompetentunit;
+	}
+	public void setRescompetentunit(String rescompetentunit) {
+		this.rescompetentunit = rescompetentunit;
+	}
+	public String getRestrade() {
+		return restrade;
+	}
+	public void setRestrade(String restrade) {
+		this.restrade = restrade;
+	}
+	public double getReslandscale() {
+		return reslandscale;
+	}
+	public void setReslandscale(double reslandscale) {
+		this.reslandscale = reslandscale;
+	}
+	public double getResvolumerate() {
+		return resvolumerate;
+	}
+	public void setResvolumerate(double resvolumerate) {
+		this.resvolumerate = resvolumerate;
+	}
+	public String getResplanuses() {
+		return resplanuses;
+	}
+	public void setResplanuses(String resplanuses) {
+		this.resplanuses = resplanuses;
+	}
+	public double getResinvest() {
+		return resinvest;
+	}
+	public void setResinvest(double resinvest) {
+		this.resinvest = resinvest;
+	}
+	public double getResgrossassets() {
+		return resgrossassets;
+	}
+	public void setResgrossassets(double resgrossassets) {
+		this.resgrossassets = resgrossassets;
+	}
+	public double getResplantime() {
+		return resplantime;
+	}
+	public void setResplantime(double resplantime) {
+		this.resplantime = resplantime;
+	}
+	public String getResplanaddress() {
+		return resplanaddress;
+	}
+	public void setResplanaddress(String resplanaddress) {
+		this.resplanaddress = resplanaddress;
+	}
+	public String getResplanideas() {
+		return resplanideas;
+	}
+	public void setResplanideas(String resplanideas) {
+		this.resplanideas = resplanideas;
+	}
+	public String getResbuildterm() {
+		return resbuildterm;
+	}
+	public void setResbuildterm(String resbuildterm) {
+		this.resbuildterm = resbuildterm;
+	}
+	public String getRespolicy() {
+		return respolicy;
+	}
+	public void setRespolicy(String respolicy) {
+		this.respolicy = respolicy;
+	}
+	public String getReselement() {
+		return reselement;
+	}
+	public void setReselement(String reselement) {
+		this.reselement = reselement;
+	}
+	public String getRescontactunit() {
+		return rescontactunit;
+	}
+	public void setRescontactunit(String rescontactunit) {
+		this.rescontactunit = rescontactunit;
+	}
+	public String getRescontacts() {
+		return rescontacts;
+	}
+	public void setRescontacts(String rescontacts) {
+		this.rescontacts = rescontacts;
+	}
+	public String getRescontactway() {
+		return rescontactway;
+	}
+	public void setRescontactway(String rescontactway) {
+		this.rescontactway = rescontactway;
+	}
+}
