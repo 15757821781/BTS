ad3cff17be13aab3e7d23d5ea6b36956f689e11e
15757821781 <594672337@qq.com>
2017-02-21 16:20:02 +0800

    招商新增功能

---
+++ TownManagement/src/main/java/com/hkay/weifei/service/InvitemService.java
@@ -0,0 +1,9 @@
+package com.hkay.weifei.service;
+
+import com.hkay.weifei.pojo.Tb_zhaoshangxiangmu;
+
+public interface InvitemService {
+
+	int insertinvitem(Tb_zhaoshangxiangmu tb_zhaoshangxiangmu);
+
+}
