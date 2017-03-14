de4f340cbcc2c982eeb93fdacad8c8922dd54609
15757821781 <594672337@qq.com>
2017-03-14 12:32:10 +0800

    将登录信息写入session

---
+++ TownManagement/src/main/java/com/hkay/weifei/util/TypeStatusConstant.java
@@ -0,0 +1,18 @@
+package com.hkay.weifei.util;
+
+/**
+ * 状态标志和常量
+ * 
+ * @author zhu
+ *
+ */
+public class TypeStatusConstant {
+	// 操作成功
+	public static final String success = "success";
+	// 操作失败
+	public static final String fail = "fail";
+	// 登出
+	public static final String login_out = "loginout";
+	// 登录信息丢失
+	public static final String session_lost = "登录信息丢失";
+}
