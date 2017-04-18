cdd34c1dc7e87975f9bfe177c37e20ce4ca0c842
15757821781 <594672337@qq.com>
2017-04-18 15:35:38 +0800

    ajax扩展

--- TownManagement/src/main/webapp/js/sideBarLoad.js
+++ TownManagement/src/main/webapp/js/sideBarLoad.js
@@ -1,14 +1,15 @@
 //@laozhu 	
 $(document).ready(function() {
-  	    $.ajax({
+  	    tk.ajax({
  	        url: "/TownManagement/wflx/loadPages",
  	       	async: false,
  	       	dataType: "json",
- 	        success: function(list){
- 	        	if(list.state=="loginout"){
- 	        		window.location.href='/TownManagement';
- 	        		return false;
- 	        	}else if(list.data.length!=0){
+ 	        succ: function(list){
+// 	        	if(list.state=="loginout"){
+// 	        		window.location.href='/TownManagement';
+// 	        		return false;
+// 	        	}else
+ 	        		if(list.data.length!=0){
  	        		$.each(list.data, function(i) {
  	        		    if(list.data[i].pagelevel=="1"){
  	        		    	$('#side-menu').append('<li><a><i class="fa fa-dashboard fa-fw"></i> '+list.data[i].pagename+'<span class="fa arrow"></span></a><ul class="nav nav-second-level" id=pageName_'+list.data[i].pageid+'></ul></li>');
--- TownManagement/src/main/webapp/js/toolKit.js
+++ TownManagement/src/main/webapp/js/toolKit.js
@@ -1,11 +1,65 @@
 var tk = tk || {};
+// success alert
+var success_content='<div class="alert alert-success" id="success_content">alert content</div>';
+// fail alert
+var fail_content='<div class="alert alert-fail" id="success_content">alert content</div>'
+tk.getFn = function(fnName, target, arg0, arg1, arg2, arg3) {
+	var fn = null;
+	var type = typeof(fnName);
+	if (target == null) {
+		target = {};
+	}
+	if (type == 'string') {// 事件以字符串方式传递一个方法名
+		fn = function(fnName, target, arg0, arg1, arg2, arg3) {
+			return function() {
+				try {
+					if (fnName.indexOf('(') != -1) {
+						return eval(fnName);
+					} else {
+						var items = fnName.split('.');
+						var length = items.length;
+						if (length == 0) {
+							return null;
+						}
+						var fun = window;
+						for (var i = 0; i < length; i++) {
+							fun = fun[items[i]];
+						}
+						return fun.call(target, arg0, arg1, arg2, arg3);
+					}
+				} catch (ee) {
+				}
+			};
+		};
+	} else if (type == 'function') {// 事件以方法方式直接传递
+		fn = function(fnName, target, arg0, arg1, arg2, arg3) {
+			return function() {
+				return fnName.call(target, arg0, arg1, arg2, arg3);
+			};
+		};
+	}
+	if (fn == null) {
+		return null;
+	}
+	return fn(fnName, target, arg0, arg1, arg2, arg3);
+};
+
+tk.execFn = function(fnName, target, arg0, arg1, arg2, arg3) {
+	var fn = tk.getFn(fnName, target, arg0, arg1, arg2, arg3);
+	if (fn != null) {
+		return fn();
+	}
+};
+
+
+
 //对ajax进行封装
 tk.ajax = function(options) {
 	options = $.extend(true, {
 		type : 'post',
 //		form : null,
 //		fields : '',
-		dataType : '',
+		dataType : 'json',
 //		validate : false,
 //		responseType : 'json',
 		showErrorMsg : true,
@@ -18,13 +72,24 @@ tk.ajax = function(options) {
 			// 登出
 			if(result.state=="loginout"){
 				// 展示登出dialog框
+				$("#sys_alert_content").html('<div class="alert alert-fail" id="alert_content">'+result.message+'</div>');
 				$("#sys_alert").modal('show');
 				// 隐藏后跳转到登录页
 				$('#sys_alert').on('hidden.bs.modal',function() {
 					window.location.href='/TownManagement';
-				})
+				});
  	        	return false;
+			} else if (result.state=="success"){
+				// 展示成功dialog框
+				$("#sys_alert_content").html('<div class="alert alert-success" id="alert_content">'+result.message+'</div>');
+				$("#sys_alert").modal('show');
+			} else if (result.state=="fail"){
+				// 展示失败dialog框
+				$("#sys_alert_content").html('<div class="alert alert-fial" id="alert_content">'+result.message+'</div>');
+				$("#sys_alert").modal('show');
 			}
+			var realParam = result;
+			tk.execFn(options.succ, options.scope, realParam);
 		},
 		error : function(XMLHttpRequest, status, errorThrown) {
 			if (options.showErrorMsg) {
@@ -43,33 +108,5 @@ tk.ajax = function(options) {
 			} 
 		}
 	}, options);
-//	if (options.form) {
-//		var form = $(options.form);
-//		if (options.validate && !form.validateForm()) {
-//			return;
-//		}
-//		var formJson = form.form2json();
-//		$.extend(options.data, formJson);
-//	}
-//	if (options.fields) {
-//		if (options.validate == true && !tk.validateFields(options.fields)) {
-//			return;
-//		}
-//		var formJson = tk.fields2json(options.fields);
-//		$.extend(options.data, formJson);
-//	}
-//	for (var key in options.data) {
-//		if (typeof(options.data[key]) == 'object') {
-//			options.data[key] = null;
-//		}
-//	}
-//	if (options.mask && options.mask.target) {
-//
-//		$(options.mask.target).mask({
-//			message : options.mask.msg,
-//			timeout : null
-//		});
-//		options.isBlocked = true;
-//	}
 	$.ajax(options);
 };
