0f33a02afe19d4b6874383649f88b05742da5d86
cxy521 <617997215@qq.com>
2017-08-29 09:56:48 +0800

    县市情况表的录入

---
+++ TownManagement/src/main/java/com/hkay/weifei/service/impl/CitycaseServiceImpl.java
@@ -0,0 +1,47 @@
+package com.hkay.weifei.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.hkay.weifei.Dao.CitycaseDao;
+import com.hkay.weifei.pojo.Tb_xianshiqingkuang;
+import com.hkay.weifei.service.CitycaseService;
+
+@Service("CitycaseService")
+public class CitycaseServiceImpl implements CitycaseService{
+	@Resource
+	private CitycaseDao citycaseDao;
+
+	@Override
+	public int insertCityInfo(Tb_xianshiqingkuang tb_xianshiqingkuang) {
+		// TODO Auto-generated method stub
+		return this.citycaseDao.insertCityInfo(tb_xianshiqingkuang);
+	}
+
+	@Override
+	public List<Tb_xianshiqingkuang> queryCityList(Tb_xianshiqingkuang tb_xianshiqingkuang) {
+		// TODO Auto-generated method stub
+		return this.citycaseDao.queryCityList(tb_xianshiqingkuang);
+	}
+
+	@Override
+	public int queryCitycnt(Tb_xianshiqingkuang tb_xianshiqingkuang) {
+		// TODO Auto-generated method stub
+		return this.citycaseDao.queryCitycnt(tb_xianshiqingkuang);
+	}
+
+	@Override
+	public List<Tb_xianshiqingkuang> queryCityDetail(Tb_xianshiqingkuang tb_xianshiqingkuang) {
+		// TODO Auto-generated method stub
+		return this.citycaseDao.queryCityDetail(tb_xianshiqingkuang);
+	}
+
+	@Override
+	public int updateCityInfo(Tb_xianshiqingkuang tb_xianshiqingkuang) {
+		// TODO Auto-generated method stub
+		return this.citycaseDao.updateCityInfo(tb_xianshiqingkuang);
+	}
+}
