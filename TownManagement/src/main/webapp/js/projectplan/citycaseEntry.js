969bc867b7cf9b237d7a9abcc810cda3998654ee
cxy521 <617997215@qq.com>
2017-08-31 16:13:43 +0800

    县市基本情况表

--- TownManagement/src/main/webapp/js/projectplan/citycaseEntry.js
+++ TownManagement/src/main/webapp/js/projectplan/citycaseEntry.js
@@ -8,8 +8,8 @@ $(document).ready(function() {
 		autoclose : true,// 选中之后自动隐藏日期选择框
 		todayBtn : true,// 今日按钮
 		startView : 4,
-        minView: 3,
-        format: "yyyy-mm"
+        minView: 4,
+        format: "yyyy"
 	}).on('hide', function(e) {  
         // 当用户改变值的时候进行验证
 		$('#basiform').bootstrapValidator('revalidateField', 'countryyear');
@@ -23,6 +23,9 @@ $(document).ready(function() {
 	$('#basicform_submit').click(function() {
 		formSubmit('#basicform','citymanage/insertCityInfo',loadpage);
 	});
+	$('#basicform_update').click(function() {
+		formSubmit('#basicform','citymanage/updateCityInfo',"ProjectPlan/citycaseManage.html");
+	});
 	$('#basicform').bootstrapValidator({
 		message : 'This value is not valid',
 		excluded : [ ':disabled' ],
@@ -30,6 +33,145 @@ $(document).ready(function() {
 			//			valid : 'glyphicon glyphicon-ok',
 					invalid : 'glyphicon glyphicon-remove',
 					validating : 'glyphicon glyphicon-refresh'
+		},
+		fields : {
+			countryname : {
+				validators : {
+//					notEmpty : {
+//						message : '县市名称不能为空'
+//					}
+				}
+			},
+			countryyear : {
+				validators : {
+//					notEmpty : {
+//						message : '统计年度不能为空'
+//					}
+				}
+			},
+			countryprovince : {
+				validators : {
+					notEmpty : {
+						message : '省份不能为空'
+					}
+				}
+			},
+			countrycity : {
+				validators : {
+					notEmpty : {
+						message : '城市不能为空'
+					}
+				}
+			},
+			countryposition : {
+				validators : {
+					notEmpty : {
+						message : '地理位置不能为空'
+					}
+				}
+			},	
+			countryarea : {
+				validators : {
+					notEmpty : {
+						message : '行政面积不能为空'
+					},
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryallpeople : {
+				validators : {
+					notEmpty : {
+						message : '全县总人口不能为空'
+					},
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countrygdp : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryrate : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryfinance : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryrates : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryfisheries : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryindustry : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryinvest : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryratess : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countryincome : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			},
+			countrytownincome : {
+				validators : {
+					regexp : {
+						regexp : /^[0-9]+(.[0-9]{1,3})?$/,
+						message : '请输入最多3位小数的数字'
+					}
+				}
+			}
 		}
 	});
 	
---
+++ TownManagement/src/main/webapp/js/projectplan/citycaseManage.js
@@ -0,0 +1,128 @@
+$(document).ready(function() {
+	$('#citymanage').bootstrapTable({
+		url : '/TownManagement/citymanage/queryCityList',
+		method : 'get', //请求方式（*）
+		toolbar : '#toolbar', //工具按钮用哪个容器
+		striped : true, //是否显示行间隔色
+		cache : false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+		pagination : true, //是否显示分页（*）
+		sortable : false, //是否启用排序
+		sortOrder : "asc", //排序方式
+		queryParamsType : "undefined",
+		queryParams : queryParams,//传递参数（*）
+		sidePagination : "server", //分页方式：client客户端分页，server服务端分页（*）
+		pageSize : 10,
+		pageList : [ 10, 25, 50, 100 ], //可供选择的每页的行数（*）
+		search : true,
+		showToggle : true,
+		showRefresh : true,//显示刷新按钮
+		strictSearch : true,//设置为 true启用 全匹配搜索，否则为模糊搜索
+		clickToSelect : true, //是否启用点击选中行
+		height : 650, //行高，如果没有设置height属性，表格自动根据记录条数调整表格高度
+		//cardView : false, //是否显示详细视图
+		//detailView : true, //是否显示父子表
+		columns : [ {
+			field : 'countryyear',
+			title : '年度',
+			editable : true,
+			align : 'center',
+			width : '20%'
+		},{
+			field : 'countryname',
+			title : '县市名称',
+			editable : true,
+			align : 'center',
+			width : '20%'
+		},{
+			title : '归属地',
+			align : 'center',
+			width : '20%',
+            formatter:function(value,row,index){
+            	return row.countryprovince+"/"+row.countrycity;
+            }
+		},{
+            title : '操作',
+            width :	'20%',
+            field : 'operation',
+            align : 'center',
+		} ]
+	});
+	//新增弹出框
+	$('#citycaseSearch_bar').click(function() {
+		$.ajax({
+			url : "/TownManagement/pages/ProjectPlan/citycaseSearch.html",
+			cache : false,
+			success : function(html) {
+				$('#citycasebody').html(html);
+				$('#form_search')[0].reset();
+				$("#citycasemodal").modal('show');
+			}
+		});
+	});
+//查询方法
+function queryParams(params){
+	if (params.searchText == undefined) {
+		params.searchText = "";
+	}
+	var temp = { //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+		limit : params.pageSize, //页面大小
+		offset : params.offset, //页码
+		maxrows : params.limit,
+		pageindex : params.pageNumber,
+		search : encodeURI(params.searchText)
+	};
+	temp = GetFormSearchData('form_search',temp);
+	return temp;
+	}
+});
+//展示详情modal
+function querydetail(id) {
+	$.ajax({
+		url : "/TownManagement/pages/ProjectPlan/citycaseEntry.html",
+		cache : false,
+		success : function(html) {
+			$("#citycasebody").html(html);
+//			$("#basicform").remove();
+			
+//			$("#comfieldset").removeAttr("disabled");
+			tk.ajax({
+				url : "/TownManagement/citymanage/queryCityDetail",
+				data : {"countryid":id},
+				dataType : 'JSON',
+				cache : false,
+				succ : function(data, status) {
+					fillForm('#basicform',data);
+					$('#cityfieldset').attr("disabled","disabled");
+					$("#citycasemodal").modal('show');
+					$("#basicform_submit").hide();
+					$("#basicform_update").hide();
+				}
+			});
+		}
+	});
+}
+//展示修改界面
+function updateinfo(id){
+	$.ajax({
+		url : "/TownManagement/pages/ProjectPlan/citycaseEntry.html",
+		cache : false,
+		success : function(html) {
+			$("#citycasebody").html(html);
+//			$("#basicform").remove();
+			
+//			$("#comfieldset").removeAttr("disabled");
+			tk.ajax({
+				url : "/TownManagement/citymanage/queryCityDetail",
+				data : {"countryid":id},
+				dataType : 'JSON',
+				cache : false,
+				succ : function(data, status) {
+					fillForm('#basicform',data);
+					$("#citycasemodal").modal('show');
+					$("#basicform_submit").hide();
+					$("#basicform_update").show();
+				}
+			});
+		}
+	});
+}
