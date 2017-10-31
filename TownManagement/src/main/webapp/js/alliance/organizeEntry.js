f14ea03d43377064e8b90f7320dbf9e70431dc59
cxy521 <617997215@qq.com>
2017-10-31 10:03:16 +0800

    社会组织单位成立时间的修改

--- TownManagement/src/main/webapp/js/alliance/organizeEntry.js
+++ TownManagement/src/main/webapp/js/alliance/organizeEntry.js
@@ -9,8 +9,8 @@ $(document).ready(function() {
 		autoclose : true,// 选中之后自动隐藏日期选择框
 		todayBtn : true,// 今日按钮
 		startView : 4,
-        minView: 2,
-        format: "yyyy-mm-dd"
+        minView: 3,
+        format: "yyyy-mm"
 	}).on('hide', function(e) {  
         // 当用户改变值的时候进行验证
 		$('#orgform').bootstrapValidator('revalidateField', 'orgestablish');
